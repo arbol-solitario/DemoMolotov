41cf902b898d8b3a4a6460a9409f1ee2bd79e893
u1973244 <u1973244@campus.udg.edu>
2022-05-27 15:42:29 +0200

    Menú fet i estil aplicat

---
+++ js/main_menu.js
@@ -0,0 +1,23 @@
+function loadpage(page){
+    window.location.assign(page);
+}
+
+function new_game(){
+	var name = prompt("User name");
+	
+    while (name==""){
+        name = prompt("User name");
+    }
+	sessionStorage.setItem("username", name);
+	
+	loadpage("./html/phasergame.html");
+}
+
+function load_game(){
+	loadpage("./html/load.html");
+}
+
+
+function how_to_play(){
+	loadpage("./html/how_to_play.html");
+}
