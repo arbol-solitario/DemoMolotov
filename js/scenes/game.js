20d6694f36f717ac85a05a768f4197d810a3da4f
u1973244 <u1973244@campus.udg.edu>
2022-05-28 18:47:14 +0200

    més portable moviment segons arma i començat input atac

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -8,13 +8,24 @@ class GameScene extends Phaser.Scene {
     preload (){	
         this.load.spritesheet('ma_caminar','../../resources/sense_arma/caminar_ma.png',{frameWidth: 279, frameHeight: 219});
         this.load.spritesheet('ma_quiet','../../resources/sense_arma/quiet_ma.png',{frameWidth: 289, frameHeight: 224});
+        this.load.spritesheet('pistola_caminar','../../resources/pistola/caminar_pistola.png',{frameWidth: 258, frameHeight: 220});
+        this.load.spritesheet('pistola_quiet','../../resources/pistola/quiet_pistola.png',{frameWidth: 253, frameHeight: 216});
+
 	}
-	
     create (){	
         this.player = this.physics.add.sprite(400,300,'ma_caminar').setScale(0.25).refreshBody();
         this.player.setBounce(0.2);
-        this.player.direccio="quiet";
-        this.player.rotation=0.5;
+        this.player.accio="quiet";
+        this.player.velocitat=120;
+        this.player.cooldown=false;
+        var ganivet = {
+            nom: "ganivet"
+        };
+        var pistola = {
+            nom: "pistola"
+        };
+        this.player.arma=ganivet;
+
         
 
 
@@ -31,6 +42,18 @@ class GameScene extends Phaser.Scene {
             frameRate: 20,
             repeat: -1
         });
+        this.anims.create ({
+            key: 'caminar_pistola',
+            frames: this.anims.generateFrameNumbers('pistola_caminar', { start: 0, end: 19 }),
+            frameRate: 30,
+            repeat: -1
+        });
+        this.anims.create ({
+            key: 'quiet_pistola',
+            frames: this.anims.generateFrameNumbers('pistola_quiet', { start: 0, end: 19 }),
+            frameRate: 30,
+            repeat: -1
+        });
         this.player.anims.play('quiet_ma');
 
 
@@ -41,72 +64,91 @@ class GameScene extends Phaser.Scene {
         this.cursors.S=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S,true,true);
         this.cursors.W=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W,true,true);
 
+        this.input.on('pointerdown', function (pointer) {
+
+            this.disparar(pointer.x,pointer.y);
+    
+    
+        }, this);
 	}
+    
+    disparar(posx,posy){
+        if(this.player.cooldown==false){
+            this.player.cooldown=true;
+            this.player.accio="atacar";
+            var posratoli= new Phaser.Math.Vector2(posx,posy);
+            var direccio=new Phaser.Math.Vector2();
+            direccio=posratoli.substract(this.player.position);
+            //direccio.normalize();
+            console.log(direccio.x);
+        }
+    }
 	
 	update (){	
         if(this.cursors.D.isDown){
-            if(this.player.direccio=="quiet"){
-                 this.player.anims.play('caminar_ma');
+            if(this.player.accio=="quiet" || (this.player.accio=="atacar" && this.player.cooldown==false)){
+                if(this.player.arma.nom=="ganivet") this.player.anims.play('caminar_ma');
+                else if (this.player.arma.nom=="pistola") this.player.anims.play('caminar_pistola');
             }
-            this.player.direccio="dreta";
+            this.player.accio="dreta";
             if(this.cursors.S.isDown){
-                this.player.rotation=1.285;
-                this.player.setVelocityX(70.7); //perque el modul de la velocitat sigui 100
-                this.player.setVelocityY(70.7);
+                this.player.rotation=0.785;
+                this.player.setVelocityX(Math.sqrt(Math.pow(this.player.velocitat,2)/2)); //perque el modul de la velocitat sigui el que ha de ser ja que sino aniria mes ràpid en diagonal
+                this.player.setVelocityY(Math.sqrt(Math.pow(this.player.velocitat,2)/2));
             }
             else if(this.cursors.W.isDown){
-                this.player.rotation=-0.285;
-                this.player.setVelocityX(70.7); //perque el modul de la velocitat sigui 100
-                this.player.setVelocityY(-70.7);
+                this.player.rotation=-0.785;
+                this.player.setVelocityX(Math.sqrt(Math.pow(this.player.velocitat,2)/2)); 
+                this.player.setVelocityY(-Math.sqrt(Math.pow(this.player.velocitat,2)/2));
             }
             else{
-                this.player.rotation=0.5;
-                this.player.setVelocityX(100);
+                this.player.rotation=0;
+                this.player.setVelocityX(this.player.velocitat);
                 this.player.setVelocityY(0);
             }
         } 
         else if(this.cursors.A.isDown){
-            if(this.player.direccio=="quiet"){
+            if(this.player.accio=="quiet"){
                 this.player.anims.play('caminar_ma');
             }
-            this.player.direccio="esquerra";
+            this.player.accio="esquerra";
             if(this.cursors.S.isDown){
-                this.player.rotation=2.855;
-                this.player.setVelocityX(-70.7); //perque el modul de la velocitat sigui 100
-                this.player.setVelocityY(70.7);
+                this.player.rotation=2.355;
+                this.player.setVelocityX(-Math.sqrt(Math.pow(this.player.velocitat,2)/2)); 
+                this.player.setVelocityY(Math.sqrt(Math.pow(this.player.velocitat,2)/2));
             }
             else if(this.cursors.W.isDown){
-                this.player.rotation=4.45;
-                this.player.setVelocityX(-70.7); //perque el modul de la velocitat sigui 100
-                this.player.setVelocityY(-70.7);
+                this.player.rotation=4.15;
+                this.player.setVelocityX(-Math.sqrt(Math.pow(this.player.velocitat,2)/2)); 
+                this.player.setVelocityY(-Math.sqrt(Math.pow(this.player.velocitat,2)/2));
             }
             else{
-                this.player.rotation=3.64;
-                this.player.setVelocityX(-100);
+                this.player.rotation=3.14;
+                this.player.setVelocityX(-this.player.velocitat);
                 this.player.setVelocityY(0);
             }
         } 
         else if(this.cursors.S.isDown){
-            if(this.player.direccio=="quiet"){
+            if(this.player.accio=="quiet"){
                 this.player.anims.play('caminar_ma');
             }
-            this.player.direccio="avall";
-            this.player.rotation=2.07;
-            this.player.setVelocityY(100);
+            this.player.accio="avall";
+            this.player.rotation=1.57;
+            this.player.setVelocityY(this.player.velocitat);
             this.player.setVelocityX(0);
         } 
         else if(this.cursors.W.isDown){
-            if(this.player.direccio=="quiet"){
+            if(this.player.accio=="quiet"){
                 this.player.anims.play('caminar_ma');
             }
-            this.player.direccio="amunt";
-            this.player.rotation=-1.07;
-            this.player.setVelocityY(-100);
+            this.player.accio="amunt";
+            this.player.rotation=-1.57;
+            this.player.setVelocityY(-this.player.velocitat);
             this.player.setVelocityX(0);
         } 
         else{//no es presiona cap tecla de moviment
-            if (this.player.direccio!="quiet"){
-                this.player.direccio="quiet";
+            if (this.player.accio!="quiet"){
+                this.player.accio="quiet";
                 this.player.setVelocityY(0);
                 this.player.setVelocityX(0);
                 this.player.anims.play('quiet_ma');
